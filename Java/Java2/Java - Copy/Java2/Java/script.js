83975be87ac91a4c0a686229f7b80e7a79846270
Keira <KeiraJCoder@hotmail.com>
2021-08-24 11:54:01 +0100

    commit

---
+++ Java/Java2/Java - Copy/Java2/Java/script.js
@@ -0,0 +1,13 @@
+// console.log('hello world')
+
+// let number = 10;
+
+// const colour = 'blue'
+
+// console.log ('my favourite drink is ${colour}')
+
+let myName = 'Keira'
+let age = '33'
+let favouriteColour = 'purple'
+
+console.log('My name is '+myName ', I am ' +age+ ' years old')
---
+++ Java/Java2/Java/Java2/script.js
@@ -0,0 +1,68 @@
+// console.log("Meow");
+
+// let myName = "Keira"
+// // let faveDrink = "Relentless"
+
+// console.log(myName)
+
+// // console.log(`My favourite drink is ${faveDrink}`)
+
+// console.log("My name is " + myName)
+
+// // const faveDrink = () => {
+// //     console.log("I'm inside the function block")
+// // }
+
+// const myFunction = () {
+//     console.log("My name is Keira");
+// }
+
+// myFunction()
+
+// const myFunction = () => {
+//     console.log("I'm testing my function");
+// }
+
+// myFunction()
+
+
+// const addTwo = (num1, num2) => {
+//     console.log (num1 + num2);
+
+// }
+
+// addTwo (3, 5)
+
+
+// const order = (drinkOrder, size) => {
+//     console.log(`Your drink is ${drinkOrder} and its size is ${size}`);
+// }
+
+// order ()
+
+
+// const coffeeOrder = (drinkOrder, size) => {
+//     console.log (`Here is your ${size} ${drinkOrder}`);
+// }
+
+// coffeeOrder("large", "Frappacino")
+
+const coffeeOrder = (size, typeOfDrink) => {
+    console.log(`Here is your ${size} ${typeOfDrink}.`);
+    // console.log("Here is your " + size + " " + typeOfDrink + ".");
+}
+
+coffeeOrder("large", "latte")
+
+let myName = "Keira"
+
+if (myName == "Keira"){
+    console.log ("That is my name");
+
+} else {
+    console.log ("Thats not my name")
+}
+
+let names = ["Ben", "Dan", "Stuart"]
+
+console.log(names[2]);
